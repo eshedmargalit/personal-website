6b856c7df5a54d757cc5928bc08c7f9bacebb5ac
Eshed Margalit <eshed.margalit@gmail.com>
2025-08-12 12:59:59 -0700

    oops...add redirect for dprime calculator

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	redirects: async () => {
+		return [
+			{
+				source: "/dprime_calculator",
+				destination: "https://dprime-calculator.vercel.app/",
+				permanent: true,
+			},
+		];
+	},
+};
 
 export default nextConfig;
